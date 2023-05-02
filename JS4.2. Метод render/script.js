792a6ec1cd17006873db9714101519d33b653f33
vladmilowid <123388360+vladmilowid@users.noreply.github.com>
2023-05-03 00:04:30 +0300

    Add files via upload

---
+++ JS4.2. Метод render/script.js
@@ -0,0 +1,12 @@
+const MyComponent = () => (
+    <div>
+      <h1>Здравствуйте, я React!</h1>
+    </div>
+  );
+  
+  ReactDOM.render(
+    <MyComponent />,
+    document.getElementById('root')
+  );
+  
+
